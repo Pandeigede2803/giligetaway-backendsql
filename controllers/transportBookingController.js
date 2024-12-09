13121613ad29feba6cdb8320354d4d6d06c3f9e9
Pandeige2803 <dedegetto_47@yahoo.com>
2024-12-09 11:27:40 +0800

    d

--- controllers/transportBookingController.js
+++ controllers/transportBookingController.js
@@ -13,11 +13,12 @@ exports.getAllTransportBookings = async (req, res) => {
         },
         {
           model: Booking,
-          where: {
-            payment_status: {
-              [Op.in]: ['paid', 'invoiced']
-            }
-          },
+          attributes: ['id', 'booking_source', 'booking_date', 'created_at', 'updated_at','payment_status'],
+          // where: {
+          //   payment_status: {
+          //     [Op.in]: ['paid', 'invoiced']
+          //   }
+          // },
           attributes: [
             'id', 'contact_name', 'contact_phone', 'contact_passport_id', 'contact_nationality', 'contact_email', 'schedule_id', 'agent_id', 'payment_method', 'gross_total', 'total_passengers', 'adult_passengers', 'child_passengers', 'infant_passengers', 'payment_status', 'booking_source', 'booking_date', 'ticket_id', 'created_at', 'updated_at'
           ],
