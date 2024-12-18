ac04ce287e98b88967abbda33a3f1dcaed72fc51
Pandeige2803 <dedegetto_47@yahoo.com>
2024-12-18 16:21:45 +0800

    update agentComission.js ASsa

--- routes/agentComission.js
+++ routes/agentComission.js
@@ -7,7 +7,7 @@ const authenticate = require('../middleware/authenticate');
 
 // Route to get agent commissions based on month, year, and agent_id
 router.get('/comissions', authenticate, agentCommissionsController.getCommissions);
-router.get('/comissions-invoicedd', authenticate, agentCommissionsController.getCommissionsInvoiced);
+router.get('/comissions-invoiced', authenticate, agentCommissionsController.getCommissionsInvoiced);
 
 
 
