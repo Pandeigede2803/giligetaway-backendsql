40d03645c883ac4f9701e29e4cc5b7ae23a7456d
Pandeige2803 <dedegetto_47@yahoo.com>
2025-01-22 23:30:08 +0800

    m

--- routes/discountRoutes.js
+++ routes/discountRoutes.js
@@ -4,18 +4,18 @@ const discountController = require('../controllers/discountController');
 const authenticate = require('../middleware/authenticate');
 
 // Create a new discount
-router.post('/discounts',authenticate, discountController.createDiscount);
+router.post('/',authenticate, discountController.createDiscount);
 
 // Get all discounts
-router.get('/discounts',authenticate, discountController.getAllDiscounts);
+router.get('/',authenticate, discountController.getAllDiscounts);
 
 // Get a discount by ID
-router.get('/discounts/:id',authenticate, discountController.getDiscountById);
+router.get('/:id',authenticate, discountController.getDiscountById);
 
 // Update a discount
-router.put('/discounts/:id',authenticate, discountController.updateDiscount);
+router.put('/:id',authenticate, discountController.updateDiscount);
 
 // Delete a discount
-router.delete('/discounts/:id',authenticate, discountController.deleteDiscount);
+router.delete('/:id',authenticate, discountController.deleteDiscount);
 
 module.exports = router;
