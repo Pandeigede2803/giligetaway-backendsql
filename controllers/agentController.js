152c14fb942e3614a9a772f058b9711be21c7689
Pandeige2803 <dedegetto_47@yahoo.com>
2025-04-21 12:50:48 +0800

    DFGH

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -530,7 +530,7 @@ exports.requestPasswordResetLink = async (req, res) => {
 
 
     const mailOptions = {
-      from: "Gili Getaway System <" + process.env.EMAIL_NOREPLY + ">",
+      from: `${process.env.EMAIL_DOMAIN}`,
       to: agent.email,
       subject: "Password Reset Request",
       text: `Please click on the following link to reset your password: ${resetLink}`,
